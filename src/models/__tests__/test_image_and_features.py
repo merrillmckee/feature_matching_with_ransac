b0ab47ec4b7212fb49826fe7fb53abd5c3f5cc62
Merrill McKee <merrillmckee@gmail.com>
2024-10-24 12:17:47 -0500

    initial `ImageAndFeatures` and `Feature` class

---
+++ src/models/__tests__/test_image_and_features.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.models.image_and_features import Feature, ImageAndFeatures
+
+
+def test_feature():
+    feature = Feature.model_validate(Feature.Config.schema_extra["example"])
+    assert isinstance(feature, Feature)
+
+
+def test_image_and_features():
+    image_and_features = ImageAndFeatures.model_validate(ImageAndFeatures.Config.schema_extra["example"])
+    assert isinstance(image_and_features, ImageAndFeatures)
+
+
+if __name__ == "__main__":
+    pytest.main()
---
+++ src/models/image_and_features.py
@@ -0,0 +1,51 @@
+import numpy as np
+from numpydantic import NDArray, Shape
+from pydantic import BaseModel, Field
+
+
+class Feature(BaseModel):
+    x: float = Field(
+        description="x-coordinate of feature in pixels",
+    )
+    y: float = Field(
+        description="y-coordinate of feature in pixels",
+    )
+    scale: float = Field(
+        description="scale of the feature in pixels",
+        default=1.0,
+    )
+    rotation: float = Field(
+        description="rotation angle, in degrees CCW from +x-axis",
+        default=90.0,
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "x": 100,
+                "y": 200,
+                "scale": 1.0,
+                "rotation": 90.0,
+            }
+        }
+
+
+class ImageAndFeatures(BaseModel):
+    image: NDArray[Shape["* x, * y, 3 rgb"], np.uint8] = Field(
+        description="(H, W, Ch) RGB image",
+    )
+    features: list[Feature] = Field(
+        description="List of image features",
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "image": np.array(
+                    # (2, 3) RGB image of alternating black and blue pixels
+                    object=[[[0, 0, 0], [0, 0, 255], [0, 0, 0]], [[0, 0, 255], [0, 0, 0], [0, 0, 255]]],
+                    dtype=np.uint8,
+                ),
+                "features": [Feature.Config.schema_extra["example"]],
+            }
+        }
